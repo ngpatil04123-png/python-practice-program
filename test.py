8a0fd0b96aef55f85aaf1aaa1e03314d8d4d6c66
ngpatil04123-png <ngpatil04123@gmail.com>
2026-02-12 12:24:18 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+a=10
+b=30.44
+s="ullas"
+sum=a/b
+print(sum)
+print(type(a))
+print(b)
+print(type(sum))
+print(int(sum))
+print(s)
+math=int(input("Enter Math marks: "))
+science=int(input("Enter Science marks: "))
+english=int(input("Enter English marks: "))
+total=math+science+english
+print(f"Total Marks={total}")
+avg=total/3
+print(f"Avarage ={avg:.2f}")
